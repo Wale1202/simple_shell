5eca6c88bec08c1e22d4445e9f0e8fa3b47fd7ca
Wale1202 <adewalefagbodun@gmail.com>
2024-01-10 15:54:02 -0800

    new stuff

--- array_man.c
+++ array_man.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
 /**
- * arrayfree - function used to free memory
- * @arr: Argument count
- * Return: free array
+ * arrayfree - function used to free memory in the shell
+ * @arr: Argument
+ * Return: free aiirray
  */
 void arrayfree(char **arr)
 {
-	int i;
+	int j;
 
 	if (!arr)
 	{
 		return;
 	}
-	for (i = 0; arr[i]; i++)
+	for (j = 0; arr[j]; j++)
 	{
-		free(arr[i]), arr[i] = NULL;
+		free(arr[j]), arr[j] = NULL;
 	}
-	free(arr), arr = NULL;
+		free(arr), arr = NULL;
 }
 
 /**
  * array_rev - a function reverse an array
  * @arr: array
- * @len: length of arr
+ * @len: length of array
  * Return: no return
  */
 void array_rev(char *arr, int len)
 {
-	int i;
+	int j;
 	char tmp;
 
-	for (i = 0; i < (len / 2); i++)
+	for (j = 0; j < (len / 2); j++)
 	{
-		tmp = arr[i];
-		arr[i] = arr[(len - 1) - i];
-		arr[(len - 1) - i] = tmp;
+		tmp = arr[j];
+			arr[j] = arr[(len - 1) - j];
+		arr[(len - 1) - j] = tmp;
 	}
 }
