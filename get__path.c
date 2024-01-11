d6b3f42b677feb63333a541fe0f25cf7c919cafb
montybasquiart <owamathias@gmail.com>
2024-01-11 00:22:55 +0000

    updated get__path.c

--- get__path.c
+++ get__path.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _getpath - a function that path environment variables
- * @command: shell command
- * Return: Returns PATH for success, NULL for failed
+ * _getpath - a function that path environment variable
+ * @command: command
+ * Return: Returns PATH for success, NULL for failer
  */
 char *_getpath(char *command)
 {
-	char *get_cmd, char *path_env, char *dir;
-	struct stat st;
+	char *full_cmd, char *path_env, char *directory;
 	int i = 0;
+	struct stat st;
 
-	while (command[j])
+	while (command[i])
 	{
-		if (command[j] == '/')
+		if (command[i] == '/')
 		{
 			if (stat(command, &st) == 0)
 				return (strdup(command));
@@ -22,27 +22,26 @@ char *_getpath(char *command)
 		i++;
 	}
 	path_env = _getenv("PATH");
-	if (!path_env)
-	{
-		return (NULL);
-	dir = strtok(path_env, ":");
-	}
 
-	while (dir)
+	if (!path_env)
+		return (NULL);
+	directory = strtok(path_env, ":");
+
+	while (directory)
 	{
-		get_cmd = malloc(strlen(dir) + strlen(command) + 2);
-		if (get_cmd)
+		full_cmd = malloc(strlen(directory) + strlen(command) + 2);
+		if (full_cmd)
 		{
-			strcpy(get_cmd, dir);
-			strcat(get_cmd, "/");
-			strcat(get_cmd, command);
-			if (stat(get_cmd, &st) == 0)
+			strcpy(full_cmd, directory);
+			strcat(full_cmd, "/");
+			strcat(full_cmd, command);
+			if (stat(full_cmd, &st) == 0)
 			{
 				free(path_env);
-				return (get_cmd);
+				return (full_cmd);
 			}
-			free(get_cmd), get_cmd = NULL;
-			dir = strtok(NULL, ":");
+			free(full_cmd), full_cmd = NULL;
+			directory = strtok(NULL, ":");
 		}
 	}
 	free(path_env);
